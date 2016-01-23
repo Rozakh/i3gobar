7de27a04bcd007300cb683cc0af11ebbc3960fb5
Roman Zakharenko <erhadal@gmail.com>
2016-01-23 14:53:54 +0300

    Use rofi for power menu

--- power.go
+++ power.go
@@ -1,18 +1,29 @@
 package main
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func power() {
-	switch clickedButton {
-	case "1":
-		err := exec.Command("zenity", "--question", "--text=Shutdown. Are you sure?").Run()
-		if err == nil {
-			exec.Command("poweroff").Run()
+	if clickedButton == "1" {
+		echoCmd := exec.Command("echo", "-e", " Logout\n Reboot\n Poweroff")
+		rofiCmd := exec.Command("rofi", "-dmenu", "-i", "-p", "Shutdown Menu:", "-bg", "#222222", "-fg", "#888888",
+			"-hlbg", "#285577", "-hlfg", "#ffffff", "-width", "20", "-hide-scrollbar")
+		echoOut, err := echoCmd.StdoutPipe()
+		if err != nil {
+			fmt.Print(err)
 		}
-	case "3":
-		err := exec.Command("zenity", "--question", "--text=Restart. Are you sure?").Run()
-		if err == nil {
+		rofiCmd.Stdin = echoOut
+		echoCmd.Start()
+		result, _ := rofiCmd.Output()
+		switch string(result) {
+		case " Logout\n":
+			exec.Command("i3-msg", "exit").Run()
+		case " Reboot\n":
 			exec.Command("reboot").Run()
+		case " Poweroff\n":
+			exec.Command("poweroff").Run()
 		}
 	}
 }
